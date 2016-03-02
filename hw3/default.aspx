5a65fad3122ffeede0053339d327baa37756dc10
unknown <djkusz@IT13366.valdosta.edu>
2016-03-02 15:05:24 -0500

    added hw3 files

---
+++ hw3/default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="default.aspx.cs" Inherits="_default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h2>HW 3 - <a href="../default.aspx">Justin Kusz</a></h2>
+    <form id="form1" runat="server">
+    <div>
+        <div style="border-color:black;border-width:1px;border-style:solid;">
+            <p>Event Name:
+                <asp:TextBox ID="txtEventName" runat="server"></asp:TextBox>
+&nbsp;<asp:RequiredFieldValidator ID="EventNameValidator" runat="server" ControlToValidate="txtEventName" ErrorMessage="Event name required." ForeColor="Red" ValidationGroup="Event">*</asp:RequiredFieldValidator>
+&nbsp;Available Seat Numbers: First
+                <asp:TextBox ID="txtFirstSeat" runat="server" Width="35px"></asp:TextBox>
+&nbsp;<asp:RequiredFieldValidator ID="FirstSeatValidator" runat="server" ControlToValidate="txtFirstSeat" ErrorMessage="First seat number required" ForeColor="Red" ValidationGroup="Event">*</asp:RequiredFieldValidator>
+&nbsp;Last
+                <asp:TextBox ID="txtLastSeat" runat="server" Width="35px"></asp:TextBox>
+&nbsp;<asp:RequiredFieldValidator ID="LastSeatValidator" runat="server" ControlToValidate="txtLastSeat" ErrorMessage="Last seat number required" ForeColor="Red" ValidationGroup="Event">*</asp:RequiredFieldValidator>
+&nbsp;<asp:Button ID="btnMakeEvent" runat="server" Text="Make Event" OnClick="btnMakeEvent_Click" ValidationGroup="Event" />
+            &nbsp;
+            </p>
+            <asp:ValidationSummary ID="ValidationSummary1" runat="server" ForeColor="Red" ValidationGroup="Event" />
+        </div>
+        <br />
+        <asp:Label ID="lblAvailTickets" runat="server" ForeColor="Red">0</asp:Label>
+&nbsp;tickets available<br />
+        <br />
+        Name&nbsp;
+        <asp:TextBox ID="txtBuyerName" runat="server"></asp:TextBox>
+&nbsp;<asp:RequiredFieldValidator ID="BuyerNameValidator" runat="server" ControlToValidate="txtBuyerName" ErrorMessage="Must specify name" ForeColor="Red" ValidationGroup="Purchase">*</asp:RequiredFieldValidator>
+&nbsp;Age&nbsp;
+        <asp:TextBox ID="txtBuyerAge" runat="server" Width="35px"></asp:TextBox>
+&nbsp;<asp:RequiredFieldValidator ID="BuyerAgeValidator" runat="server" ControlToValidate="txtBuyerAge" ErrorMessage="Must specify age" ForeColor="Red" ValidationGroup="Purchase">*</asp:RequiredFieldValidator>
+&nbsp;Seat&nbsp;
+        <asp:DropDownList ID="ddlSeats" runat="server">
+        </asp:DropDownList>
+&nbsp;<asp:RequiredFieldValidator ID="SeatNumValidator" runat="server" ControlToValidate="ddlSeats" ErrorMessage="Must specify seat number" ForeColor="Red" ValidationGroup="Purchase">*</asp:RequiredFieldValidator>
+&nbsp;<asp:Button ID="btnPurchase" runat="server" Text="Purchase" Enabled="False" ValidationGroup="Purchase" />
+&nbsp;
+        <asp:Button ID="btnSummary" runat="server" Text="Event Summary" Enabled="False" />
+
+        <asp:ValidationSummary ID="ValidationSummary2" runat="server" ForeColor="Red" ValidationGroup="Purchase" />
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ hw3/default.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _default : System.Web.UI.Page
+{
+    int availSeats;
+    List<Seat> seats;
+    ListItem[] seatNums;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            seatNums = (ListItem[])Session["SeatNums"];
+            seats = (List<Seat>)Session["Seats"];
+            availSeats = (int)Session["AvailSeats"];
+            
+        }
+        else
+        {
+            availSeats = 0;
+            Session["AvailSeats"] = availSeats;
+        }
+        lblAvailTickets.Text = availSeats.ToString();
+    }
+    protected void btnMakeEvent_Click(object sender, EventArgs e)
+    {
+        String eventName = txtEventName.Text;
+        int firstSeat = Convert.ToInt32(txtFirstSeat.Text);
+        int lastSeat = Convert.ToInt32(txtLastSeat.Text);
+        int numSeats = (lastSeat - firstSeat) + 1;
+        seatNums = new ListItem[numSeats];
+        for (int i = 0; i < numSeats; i++)
+        {
+            seatNums[i] = new ListItem((i + firstSeat).ToString());
+        }
+        ddlSeats.DataSource = seatNums;
+        ddlSeats.DataBind();
+        Session["SeatNums"] = seatNums;
+        Session["AvailSeats"] = numSeats;
+        btnPurchase.Enabled = true;
+        btnSummary.Enabled = true;
+        lblAvailTickets.Text = numSeats.ToString();
+    }
+    protected void btnPurchase_Click(object sender, EventArgs e)
+    {
+        String buyerName = txtBuyerName.Text;
+        int buyerAge = Convert.ToInt32(txtBuyerAge.Text);
+        int seatNum = Convert.ToInt32(ddlSeats.SelectedItem.Text);
+        
+        seats.Add(new Seat(buyerName, seatNum, buyerAge));
+        Session["Seats"] = seats;
+    }
+}
+
+/*public class MyEvent
+{
+    private String name;
+    private int availSeats;
+    private List<Seat> seats;
+
+    public MyEvent(String name, int numSeats)
+    {
+        this.name = name;
+        this.availSeats = numSeats;
+        this.seats = new List<Seat>();
+    }
+
+    public String Name
+    {
+        get { return name; }
+    }
+
+    public int AvailSeats
+    {
+        get { return availSeats; }
+    }
+
+    public List<Seat> Seats
+    {
+        get { return seats; }
+    }
+
+    public bool PurchaseSeat(Seat seat)
+    {
+        if (availSeats > 0)
+        {
+            seats.Add(seat);
+            availSeats--;
+            return true;
+        }
+        return false;
+    }
+}*/
+
+public class Seat
+{
+    private String name;
+    private int number;
+    private int age;
+    private double price;
+
+    public Seat(String name, int number, int age)
+    {
+        this.name = name;
+        this.number = number;
+        this.age = age;
+
+        if (age < 13)
+        {
+            this.price = 5.00;
+        }
+        else
+        {
+            this.price = 10.00;
+        }
+    }
+
+    public String Name
+    {
+        get { return name; }
+    }
+
+    public int Number
+    {
+        get { return number; }
+    }
+
+    public int Age{
+        get { return age; }
+    }
+
+    public double Price{
+        get { return price; }
+    }
+}
