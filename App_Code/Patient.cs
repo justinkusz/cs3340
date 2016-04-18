17163beb40153857fe000138b1f533f155ef9257
justinkusz <justin.kusz@gmail.com>
2016-04-18 12:54:01 -0400

    Started Patient and Visit classes

---
+++ App_Code/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Patient
+/// </summary>
+public class Patient
+{
+    private string lastName;
+    private string firstName;
+    private string address;
+	public Patient(string lastName, string firstName, string address)
+	{
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.address = address;
+	}
+
+
+}
---
+++ App_Code/Visit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Visit
+/// </summary>
+public class Visit
+{
+    string date;
+    double charge;
+    string notes;
+
+	public Visit(string date, double charge, string notes)
+	{
+        this.date = date;
+        this.charge = charge;
+        this.notes = notes;
+	}
+
+    public string Date
+    {
+        get { return date; }
+    }
+
+    public double Charge
+    {
+        get { return charge; }
+    }
+
+    public string Notes
+    {
+        get { return notes; }
+    }
+}
