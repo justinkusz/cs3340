60bd5411e40d53c82294fd58ff1a783e755f4e30
justinkusz <justin.kusz@gmail.com>
2016-04-27 14:41:37 -0400

    Started HW6

---
+++ hw6/hw6/hw6/DataParserService.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace hw6
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : DataParserService
+    {
+        public double[] ParseData(string data)
+        {
+            string[] numbers = data.Split(' ');
+
+            double[] result = new double[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result[i] = Convert.ToDouble(numbers[i]);
+            }
+            return result;
+        }
+    }
+}
---
+++ hw6/hw6/hw6_app/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="hw6_app.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>HW 6 - <a href="../index.html">Justin Kusz</a></h1>
+        <p>Type in data seperated by spaces</p>
+        <p>
+            <asp:TextBox ID="txtInputData" runat="server" Height="45px" Width="509px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="btnCalculate" runat="server" OnClick="btnCalculate_Click" Text="Calculate" />
+        </p>
+        <p>
+            <asp:TextBox ID="txtResults" runat="server" Height="139px" TextMode="MultiLine" Width="506px"></asp:TextBox>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
